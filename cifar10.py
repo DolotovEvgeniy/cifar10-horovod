736f0f6e86054bdfbce583460fbbf9cd0d75f8c1
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2020-08-20 13:00:42 +0300

    Add cifar10 dataset

---
+++ cifar10.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.datasets.cifar import load_batch
+
+
+def data():
+    """
+    https://github.com/tensorflow/tensorflow/blob/v2.3.0/tensorflow/python/keras/datasets/cifar10.py
+    """
+    path = 'cifar-10/cifar-10-batches-py'
+
+    num_train_samples = 50000
+
+    x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train = np.empty((num_train_samples,), dtype='uint8')
+
+    for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000:i * 10000, :, :, :],
+         y_train[(i - 1) * 10000:i * 10000]) = load_batch(fpath)
+
+    fpath = os.path.join(path, 'test_batch')
+    x_test, y_test = load_batch(fpath)
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+
+    if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
+
+    x_test = x_test.astype(x_train.dtype)
+    y_test = y_test.astype(y_train.dtype)
+
+    return (x_train, y_train), (x_test, y_test)
